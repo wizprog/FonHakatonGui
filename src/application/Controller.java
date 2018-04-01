3ce8c0b9d07dc236718f3cd00df02580da71a51a
Petar Nojner <nojner.petar@gmail.com>
2018-04-01 04:32:28 +0200

    Noki commit 2 fon

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import sqlConnection.*;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
 import javafx.event.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,24 +13,45 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 import javafx.scene.control.*;
 
-public class Controller implements EventHandler<ActionEvent>{
+public class Controller implements EventHandler<ActionEvent> {
+
+	@FXML
+	private ImageView img1;
+	@FXML
+	private Label lbl1;
+	@FXML
+	private ImageView img2;
+	@FXML
+	private Label lbl2;
+	@FXML
+	private ImageView img3;
+	@FXML
+	private Text txt1;
+	@FXML
+	private Text txt2;
+	@FXML
+	private Text txt3;
+
+	private RotateTransition rotateTransition1, rotateTransition2, rotateTransition3;
 
 	@FXML
 	private TextField username;
-	
+
 	@FXML
 	private PasswordField password;
-	
+
 	@FXML
 	private Label label;
 
@@ -41,51 +64,88 @@ public class Controller implements EventHandler<ActionEvent>{
 	public void onMouseEntered(MouseEvent event) {
 		label.setTextFill(Color.color(0.465, 0, 0));
 	}
-	
+
 	@FXML
 	public void onMouseExited(MouseEvent event) {
-		label.setTextFill(Color.color(0.565, 0.565,0.565));
+		label.setTextFill(Color.color(0.565, 0.565, 0.565));
 	}
-	
+
 	double xOffset = 0, yOffset = 0;
 
 	@FXML
 	private void handleButtonActionLogIn(ActionEvent event) throws Exception {
 		// Database dat = new Database();
-	        
-	     //   User current = dat.queryLog(username.getText(), password.getText());
-			
-	     
-				Parent signInPage = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
-				Scene signInScene = new Scene(signInPage);
-				Stage logIn = (Stage)((Node)event.getSource()).getScene().getWindow();
-				logIn.setScene(signInScene);
-				 //  if (current != null) {
-					   logIn.show();
-				//   }
 
-			//dat.closeConnection();
-			
-		signInPage.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				xOffset = event.getSceneX();
-				yOffset = event.getSceneY();
-			}
+		// User current = dat.queryLog(username.getText(), password.getText());
+
+		img1.setImage(new Image("picture/syn.png"));
+		txt1.setText("Checking username");
+		rotateTransition1 = new RotateTransition(Duration.seconds(2), img1);
+		rotateTransition2 = new RotateTransition(Duration.seconds(2), img2);
+		rotateTransition3 = new RotateTransition(Duration.seconds(2), img3);
+		RotateTransition transition[] = { rotateTransition1, rotateTransition2, rotateTransition3 };
+		for (RotateTransition rTransition : transition) {
+			rTransition.setCycleCount(1);
+			rTransition.setAutoReverse(false);
+			rTransition.setFromAngle(720);
+			rTransition.setToAngle(0);
+		}
+		rotateTransition1.play();
+		rotateTransition1.setOnFinished((e) -> {
+			img1.setImage(new Image("picture/ok.png"));
+			lbl1.setStyle("-fx-background-color:#45A563");
+			img2.setImage(new Image("picture/syn.png"));
+			txt2.setText("Checking password");
+			// function for checking if valid
+			rotateTransition2.play();
 		});
 
-		// move around here
-		signInPage.setOnMouseDragged(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				logIn.setX(event.getScreenX() - xOffset);
-				logIn.setY(event.getScreenY() - yOffset);
-			}
+		rotateTransition2.setOnFinished((e) -> {
+			img2.setImage(new Image("picture/ok.png"));
+			lbl2.setStyle("-fx-background-color:#45A563");
+			img3.setImage(new Image("picture/syn.png"));
+			txt3.setText("Accessing");
+			// function for checking if valid
+			rotateTransition3.play();
 		});
-		logIn.setScene(signInScene);
-		logIn.show();
+		rotateTransition3.setOnFinished((e) -> {
+			img3.setImage(new Image("picture/ok.png"));
+			Parent signInPage = null;
+			try {
+				signInPage = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			Scene signInScene = new Scene(signInPage);
+			Stage logIn = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			logIn.setScene(signInScene);
+			// if (current != null) {
+			logIn.show();
+			// }
+
+			// dat.closeConnection();
+
+			signInPage.setOnMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					xOffset = event.getSceneX();
+					yOffset = event.getSceneY();
+				}
+			});
+
+			// move around here
+			signInPage.setOnMouseDragged(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					logIn.setX(event.getScreenX() - xOffset);
+					logIn.setY(event.getScreenY() - yOffset);
+				}
+			});
+			logIn.setScene(signInScene);
+			logIn.show();
+		});
 	}
-	
+
 	@FXML
 	private void handleButtonActionLogUp(ActionEvent event) throws Exception {
 		Parent signUpPage = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
@@ -109,6 +169,7 @@ public class Controller implements EventHandler<ActionEvent>{
 		});
 		logUp.setScene(signUpScene);
 		logUp.show();
+
 	}
 
 	@Override
--- src/application/FXMLSignInController.java
+++ src/application/FXMLSignInController.java
@@ -19,7 +19,9 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.web.WebEngine;
@@ -27,6 +29,7 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
+import java.net.URL;
 
 public class FXMLSignInController implements EventHandler<ActionEvent> {
 
@@ -36,46 +39,50 @@ public class FXMLSignInController implements EventHandler<ActionEvent> {
 	private Label label;
 	@FXML
 	private WebView browser;
-	
+	@FXML
+	private Label close;
+
+	MyBrowser myBrowser;
+
 	private AnchorPane homePane;
-	
+
 	@FXML
 	private Button homeBtn;
 	@FXML
 	private ImageView img1;
-	
+
 	private RotateTransition rotateTransition1;
 	public Boolean ready = false;
-	int i=0;
-	
+	int i = 0;
+
 	@Override
 	public void handle(ActionEvent arg0) {
 
 	}
-	
+
 	@FXML
 	private void handleClose(MouseEvent event) {
 		System.exit(0);
 	}
-	
+
 	@FXML
 	private void minimizeApp(MouseEvent event) {
-		Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		stage.setIconified(true);
 	}
-	
-	 public void setNode(Node node) {
+
+	public void setNode(Node node) {
 		pane.getChildren().clear();
-		pane.getChildren().add((Node)node);
-		
+		pane.getChildren().add((Node) node);
+
 		ft(node);
 	}
-	
-	public void createPage(AnchorPane home, String loc) throws IOException{
+
+	public void createPage(AnchorPane home, String loc) throws IOException {
 		home = FXMLLoader.load(getClass().getResource(loc));
 		setNode(home);
 	}
-	
+
 	private void ft(Node node) {
 		FadeTransition ft = new FadeTransition(Duration.seconds(1.5));
 		ft.setNode(node);
@@ -85,40 +92,71 @@ public class FXMLSignInController implements EventHandler<ActionEvent> {
 		ft.setCycleCount(1);
 		ft.play();
 	}
-	
+
 	@FXML
 	private void sceneActionHome(ActionEvent event) throws IOException {
 		this.createPage(homePane, "/application/home.fxml");
 	}
-	
+
 	@FXML
 	private void sceneActionPay(ActionEvent event) throws IOException {
 		this.createPage(homePane, "/application/onlinePaying.fxml");
 	}
-	
+
 	@FXML
-	private void sceneActionPromotions(ActionEvent event) throws IOException{
+	private void sceneActionPromotions(ActionEvent event) throws IOException {
 		this.createPage(homePane, "/application/loading.fxml");
 		// salje se serveru poruka
-		
-		//this.createPage(homePane, "/application/promotions.fxml");
+
+		// this.createPage(homePane, "/application/promotions.fxml");
 	}
-	
+
 	@FXML
 	private void sceneActionGo(ActionEvent event) throws IOException {
+
+		myBrowser = new MyBrowser();
+		browser = myBrowser.webView;
 		this.createPage(homePane, "/application/go.fxml");
-		browser = new WebView();
-		WebEngine webEngine = browser.getEngine();
-		webEngine.load("http://google.com");
+
 	}
-	
+
+	static {
+		System.setProperty("java.net.useSystemProxies", "true");
+	}
+
 	// videcemo kako ce ovo da se salje
-	
+
 	private void check() {
-		if(Integer.parseInt(label.getText())>0 && label.getText()!=null) {
+		if (Integer.parseInt(label.getText()) > 0 && label.getText() != null) {
 			label.setVisible(true);
 		}
 	}
-	
+
+	class MyBrowser extends Region {
+		HBox toolbar;
+
+		WebView webView = new WebView();
+		WebEngine webEngine = webView.getEngine();
+
+		public MyBrowser() {
+
+			final URL urlGoogleMaps = getClass().getResource("GoogleMapsV3.html");
+			webEngine.load(urlGoogleMaps.toExternalForm());
+			webEngine.setJavaScriptEnabled(true);
+
+			getChildren().add(webView);
+
+		}
+	}
+
+	@FXML
+	public void onMouseEntered(MouseEvent event) {
+		close.setTextFill(Color.color(0.465, 0, 0));
+	}
+
+	@FXML
+	public void onMouseExited(MouseEvent event) {
+		close.setTextFill(Color.color(0.565, 0.565, 0.565));
+	}
 
 }
